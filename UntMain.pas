6bc4dce58ee5bb06a46baa8793af6e8f48ef06cf
Jeihcio Francis <jeihcio.francis@hotmail.com>
2020-08-18 12:28:27 -0300

    Adicionando os ícones

--- UntMain.pas
+++ UntMain.pas
@@ -4,11 +4,13 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, dxGDIPlusClasses;
 
 type
   TForm1 = class(TForm)
     Panel1: TPanel;
+    ImgFechar: TImage;
+    ImgMinimizar: TImage;
   private
   public
   end;
